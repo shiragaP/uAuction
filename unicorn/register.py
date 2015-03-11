5d34b666180dfae562611bc66597612b4480490e
shiragaP <shiragaP@yahoo.com>
2015-03-11 12:10:05 +0700

    rebase

--- unicorn/register.py
+++
@@ -1,8 +0,0 @@
-
-
-## To waterslider
-
-# create widget ui from ui/register.ui
-# link every lineEdit to string
-# when button pressed validate for each string according to example and explaination
-# have bool to store for validation
