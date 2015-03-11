2f48fe50643689fe32c7222bc4893964e6364ba7
shiragaP <shiragaP@yahoo.com>
2015-03-11 14:02:42 +0700

    add new User.py

---
+++ unicorn/User.py
@@ -0,0 +1,15 @@
+__author__ = 'Shiraga-P'
+
+import psycopg2
+
+class User:
+
+    def __init__(self, id, username, password):
+        try:
+            conn = psycopg2.connect("dbname='database SE Y2' user='postgres' host='localhost' password='admin' port='5432'")
+        except:
+            print('I am unable to connect to the database')
+            return
+        self.id = id
+        self.username = username
+        self.password = password
