fcbf0f190d20150ee26555742a8734f3a9160632
shiragaP <shiragaP@yahoo.com>
2015-03-11 11:46:50 +0700

    finish demo for login

--- unicorn/login.py
+++ unicorn/login.py
@@ -7,14 +7,21 @@ from PySide import QtUiTools
 
 class LoginWidget(QtGui.QWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, DEBUGMODE=False):
         super().__init__(parent)
+        self.DEBUGMODE = DEBUGMODE
 
         loader = QtUiTools.QUiLoader(self)
         form = loader.load('ui\\login.ui')
 
         self.label_logo = form.findChild(QtGui.QLabel, 'label_00_logo')
         self.label_logo.setPixmap(QtGui.QPixmap('..\\resources\\img\\logo.png'))
+
+        self.lineEdit_username = form.findChild(QtGui.QLineEdit, 'lineEdit_01_username')
+        self.lineEdit_password = form.findChild(QtGui.QLineEdit, 'lineEdit_02_password')
+        self.lineEdit_password.setEchoMode(QtGui.QLineEdit.Password)
+        self.pushButton_login = form.findChild(QtGui.QPushButton, 'pushButton_login')
+        self.pushButton_login.clicked.connect(self.loginAction)
 
         layout = QtGui.QGridLayout()
         layout.addWidget(form)
@@ -24,9 +31,14 @@ class LoginWidget(QtGui.QWidget):
         self.setFixedWidth(form.width() + 15)
         self.setFixedHeight(form.height() + 15)
 
+    def loginAction(self):
+        if(self.DEBUGMODE):
+            print("Login")
+            print("\tUsername: " + self.lineEdit_username.text())
+            print("\tPassword: " + self.lineEdit_password.text())
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    loginWidget = LoginWidget()
+    loginWidget = LoginWidget(DEBUGMODE=True)
     loginWidget.show()
     sys.exit(app.exec_())
