70b8d514f1e52eb1d54a21552b2bc7b744c9a3f9
waterstrider <waterstrider.vin@gmail.com>
2015-04-13 02:55:50 +0700

    reformat

--- griffon/Auction.py
+++ griffon/Auction.py
@@ -1,20 +1,29 @@
 __author__ = 'Shiraga-P'
 
-import psycopg2
-
-import DatabaseInfo
+import pickle
+import http.client
+import urllib.parse
+import tempfile
 
 
 class Auction():
     def __init__(self, auction_id):
         self.auction_id = auction_id
 
-        conn = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" %
-                                (DatabaseInfo.host, DatabaseInfo.dbname, DatabaseInfo.user, DatabaseInfo.password))
-        cur = conn.cursor()
+        # TODO: make connection not localhost
+        conn = http.client.HTTPConnection("localhost", 8080)
 
-        cur.execute("SELECT * from auctions WHERE auctions.id=%s", (self.auction_id,))
-        row = cur.fetchall()[0]
+        # params = urllib.parse.urlencode({'auction_id':auction_id})
+        # headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+        # conn.request("POST", "/auction", params, headers)
+        params = urllib.parse.urlencode({'statement': "SELECT * from auctions WHERE auctions.id=%s" % (auction_id,)})
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+        conn.request("POST", "/query", params, headers)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+
+        row = pickle.loads(data)[0]
         self.name = row[1]
         self.seller_id = row[2]
         self.buyoutavailable = row[3]
@@ -25,15 +34,22 @@ class Auction():
         self.thumbnailpath = row[8]
         self.expirytime = row[9]
         self.soldout = row[10]
-        self.imagepathes = list()
 
-        cur.execute("SELECT * from auction_images WHERE auctionid=%s", (self.auction_id,))
-        rows = cur.fetchall()
-        for row in rows:
-            self.imagepathes += (row[1],)
+        conn.request("POST", "/query", params, headers)
+        response = conn.getresponse()
+        data = response.read()
 
-        conn.commit()
-        cur.close()
+        #TODO: change imagelist to imagepaths
+        imageurls = pickle.loads(data)
+        self.imagepaths = list()
+        for imageurl in imageurls:
+            conn.request("GET", imageurl[1])
+            response = conn.getresponse()
+            temp = tempfile.TemporaryFile()
+            temp.write(response.read())
+            self.imagepaths.append(temp)
+            print(tempfile.gettempdir(), temp.name)
+
         conn.close()
 
     def printInfo(self):
@@ -46,6 +62,8 @@ class Auction():
         print("Description: " + str(self.description))
         print("Thumbnail Path: " + str(self.thumbnailpath))
         print("Expirytime: " + str(self.expirytime))
+        print("Soldout: " + str(self.soldout))
+        print("imagepaths: " + str(self.imagepaths))
 
 
 if __name__ == '__main__':
--- griffon/webserver.py
+++ griffon/webserver.py
@@ -3,19 +3,23 @@
 # Copyright Jon Berg , turtlemeat.com
 # Modified by nikomu @ code.google.com     
 
-import string
+import pickle
 import cgi
 import time
 from os import curdir, sep
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os  # os. path
 
+import psycopg2
+
+import DatabaseInfo
+
 
 CWD = os.path.abspath('.')
 ## print CWD
 
 # PORT = 8080
-UPLOAD_PAGE = 'upload.html'  # must contain a valid link with address and port of the server     s
+UPLOAD_PAGE = 'upload.html'  # must contain a valid link with address and port of the servers
 
 
 def make_index(relpath):
@@ -46,13 +50,23 @@ def make_index(relpath):
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-
+            print(self.path)
             if self.path == '/':
                 page = make_index('.')
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                self.wfile.write(page)
+                self.wfile.write(page.encode('ascii'))
+                return
+
+            if self.path.endswith(".png") or self.path.endswith(".jpg"):
+                f = open(self.path, "rb")
+                print(f.name)
+                self.send_response(200)
+                self.send_header('Content-type', 'application/octet-stream')
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
                 return
 
             if self.path.endswith(".html"):
@@ -63,7 +77,7 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                self.wfile.write(f.read())
+                self.wfile.write(f.read().encode('ascii'))
                 f.close()
                 return
 
@@ -77,15 +91,15 @@ class MyHandler(BaseHTTPRequestHandler):
 
             else:  # default: just send the file
 
-                filepath = self.path[1:]  # remove leading '/'
-
-                f = open(os.path.join(CWD, filepath), 'rb')
+                # filepath = self.path[1:]  # remove leading '/'
+                f = open(self.path, 'rb')
+                # f = open(os.path.join(CWD, filepath), 'rb')
                 # note that this potentially makes every file on your computer readable by the internet
 
                 self.send_response(200)
                 self.send_header('Content-type', 'application/octet-stream')
                 self.end_headers()
-                self.wfile.write(f.read().encode('ascii'))
+                self.wfile.write(f.read())
                 f.close()
                 return
 
@@ -95,14 +109,14 @@ class MyHandler(BaseHTTPRequestHandler):
             print("IOError: ", e)
             self.send_error(404, 'File Not Found: %s' % self.path)
 
-
     def do_POST(self):
         # global rootnode ## something remained in the orig. code     
         try:
             ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
-
+            print(ctype)
+            print(pdict)
+            print(self.path)
             if ctype == 'multipart/form-data':
-
                 # original version :     
                 '''
                 query=cgi.parse_multipart(self.rfile, pdict)
@@ -117,36 +131,79 @@ class MyHandler(BaseHTTPRequestHandler):
                                       # but as the FieldStorage object was designed for CGI, absense of 'POST' value in environ
                                       # will prevent the object from using the 'fp' argument !
                                       )
-                ## print 'have fs'
+                # print 'have fs'
 
+                fs_up = fs['upfile']
+                print(fs_up)
+                filename = os.path.split(fs_up.filename)[1]  # strip the path, if it presents
+                fullname = os.path.join(CWD, filename)
+
+                # check for copies :
+                if os.path.exists(fullname):
+                    fullname_test = fullname + '.copy'
+                    i = 0
+                    while os.path.exists(fullname_test):
+                        fullname_test = "%s.copy(%d)" % (fullname, i)
+                        i += 1
+                    fullname = fullname_test
+
+                if not os.path.exists(fullname):
+                    with open(fullname, 'wb') as o:
+                        # self.copyfile(fs['upfile'].file, o)
+                        o.write(fs_up.file.read())
+
+                self.send_response(200)
+                self.end_headers()
+
+                self.wfile.write("<HTML><HEAD></HEAD><BODY>POST OK.<BR><BR>".encode('ascii'))
+                self.wfile.write(("File uploaded under name: " + os.path.split(fullname)[1]).encode('ascii'))
+                self.wfile.write(('<BR><A HREF=%s>back</A>' % (UPLOAD_PAGE, )).encode('ascii'))
+                self.wfile.write("</BODY></HTML>".encode('ascii'))
+
+            elif ctype == 'application/x-www-form-urlencoded':
+                if self.path == '/query':
+                    fs = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD': 'POST'})
+                    statement = fs['statement'].value
+                    conn = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (
+                    DatabaseInfo.host, DatabaseInfo.dbname, DatabaseInfo.user, DatabaseInfo.password))
+                    cur = conn.cursor()
+                    cur.execute(statement)
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(pickle.dumps(cur.fetchall()))
+                    conn.commit()
+                    cur.close()
+                    conn.close()
+
+                # elif self.path == '/auction':
+                # fs = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD': 'POST'})
+                #     auction_id = fs['auction_id'].value
+                #     conn = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (DatabaseInfo.host, DatabaseInfo.dbname, DatabaseInfo.user, DatabaseInfo.password))
+                #     cur = conn.cursor()
+                #     cur.execute("SELECT * from auctions WHERE auctions.id=%s", (auction_id,))
+                #     self.send_response(200)
+                #     self.end_headers()
+                #     self.wfile.write(pickle.dumps(cur.fetchall()))
+                #     conn.commit()
+                #     cur.close()
+                #     conn.close()
+                # elif self.path == '/auction_images':
+                #     fs = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD': 'POST'})
+                #     auction_id = fs['auction_id'].value
+                #     conn = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (DatabaseInfo.host, DatabaseInfo.dbname, DatabaseInfo.user, DatabaseInfo.password))
+                #     cur = conn.cursor()
+                #     cur.execute("SELECT * from auction_images WHERE auction_images.id=%s", (auction_id,))
+                #     self.send_response(200)
+                #     self.end_headers()
+                #     self.wfile.write(pickle.dumps(cur.fetchall()))
+                #     conn.commit()
+                #     cur.close()
+                #     conn.close()
+
+                else:
+                    raise Exception("Unexpected request path")
             else:
                 raise Exception("Unexpected POST request")
-
-            fs_up = fs['upfile']
-            filename = os.path.split(fs_up.filename)[1]  # strip the path, if it presents
-            fullname = os.path.join(CWD, filename)
-
-            # check for copies :     
-            if os.path.exists(fullname):
-                fullname_test = fullname + '.copy'
-                i = 0
-                while os.path.exists(fullname_test):
-                    fullname_test = "%s.copy(%d)" % (fullname, i)
-                    i += 1
-                fullname = fullname_test
-
-            if not os.path.exists(fullname):
-                with open(fullname, 'wb') as o:
-                    # self.copyfile(fs['upfile'].file, o)
-                    o.write(fs_up.file.read())
-
-            self.send_response(200)
-            self.end_headers()
-
-            self.wfile.write("<HTML><HEAD></HEAD><BODY>POST OK.<BR><BR>".encode('ascii'))
-            self.wfile.write(("File uploaded under name: " + os.path.split(fullname)[1]).encode('ascii'))
-            self.wfile.write(('<BR><A HREF=%s>back</A>' % (UPLOAD_PAGE, )).encode('ascii'))
-            self.wfile.write("</BODY></HTML>".encode('ascii'))
 
         except Exception as e:
             print("Exception: ", e)
@@ -155,7 +212,11 @@ class MyHandler(BaseHTTPRequestHandler):
 
 def main():
     try:
-        server = HTTPServer(('161.246.94.43', 8080), MyHandler)
+        import socket
+
+        print(socket.gethostbyname(socket.gethostname()))
+        # server = HTTPServer((socket.gethostbyname(socket.gethostname()), 8080), MyHandler)
+        server = HTTPServer(('localhost', 8080), MyHandler)
         print('started httpserver...')
         server.serve_forever()
     except KeyboardInterrupt:
